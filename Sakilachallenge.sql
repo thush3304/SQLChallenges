654c013d68d9f117391acd9ee894907a2b446c2f
thush3304 <86304577+thush3304@users.noreply.github.com>
2021-06-29 14:15:52 +0100

    Sakila challenge

---
+++ Sakilachallenge.sql
@@ -0,0 +1,6 @@
+USE sakila; -- Challenge Sakila
+-- SELECT * from actor; Question 1
+-- SELECT last_name from actor WHERE first_name = 'John'; Question 2
+-- SELECT * from actor WHERE last_name = 'Neeson'; Question 3
+-- SELECT * from actor WHERE actor_id%10=0; Question 4
+-- SELECT description from film_text WHERE film_id=100; Question 5
