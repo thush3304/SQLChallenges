0a47f8076730dd1f564110df6401219466bb730a
Thushith Premadas <86304577+thush3304@users.noreply.github.com>
2021-06-29 14:47:38 +0100

    Updated Sakila q9

--- Sakilachallenge.sql
+++ Sakilachallenge.sql
@@ -4,3 +4,7 @@ USE sakila; -- Challenge Sakila
 -- SELECT * from actor WHERE last_name = 'Neeson'; Question 3
 -- SELECT * from actor WHERE actor_id%10=0; Question 4
 -- SELECT description from film_text WHERE film_id=100; Question 5
+-- SELECT title from film WHERE rating='R'; Question 6
+-- SELECT title from film WHERE rating!='R'; Question 7
+-- SELECT title from film ORDER BY length ASC LIMIT 10; Question 8
+-- SELECT title, length from film WHERE length=(SELECT MAX(length) from film); Question 9
