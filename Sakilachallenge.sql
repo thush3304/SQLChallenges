f8bf5f5fa3080b7e12433883b9e6dab6d620a85a
Thushith Premadas <86304577+thush3304@users.noreply.github.com>
2021-06-30 11:06:20 +0100

    Finished sakila tasks

--- Sakilachallenge.sql
+++ Sakilachallenge.sql
@@ -8,3 +8,44 @@ USE sakila; -- Challenge Sakila
 -- SELECT title from film WHERE rating!='R'; Question 7
 -- SELECT title from film ORDER BY length ASC LIMIT 10; Question 8
 -- SELECT title, length from film WHERE length=(SELECT MAX(length) from film); Question 9
+-- SELECT title, special_features from film WHERE special_features LIKE '%Deleted Scenes%'; Question 10
+-- SELECT last_name FROM actor GROUP BY last_name HAVING  COUNT(last_name)=1 ORDER BY last_name DESC; Question 11
+-- SELECT last_name FROM actor GROUP BY last_name HAVING COUNT(last_name)>1 ORDER BY last_name ASC; Question 12
+/*
+SELECT a.actor_id, a.first_name, a.last_name
+FROM actor a
+JOIN film_actor f ON a.actor_id=f.actor_id
+group by a.actor_id, a.last_name
+order by count(*) desc ;
+*/ -- Question 13
+-- SELECT release_year FROM film WHERE title='Academy Dinosaur'; Question 14
+-- SELECT AVG(length) FROM film; Question 15
+ /*
+ SELECT AVG(length)
+ FROM film f
+ JOIN film_category fc ON f.film_id=fc.film_id
+ GROUP BY fc.category_id;
+ */ -- Question 16
+ -- SELECT * FROM film WHERE description LIKE '%robot%'; Question 17
+ 
+ -- SELECT COUNT(release_year)FROM film WHERE release_year=2010 ; Question 18
+ 
+ /*SELECT title FROM film f JOIN film_category fc ON f.film_id=fc.film_id 
+ -WHERE fc.category_id = (SELECT category_id FROM category WHERE name = 'HORROR');*/ -- Question 19
+ 
+ -- SELECT first_name , last_name FROM staff WHERE staff_id=2; Question 20
+ 
+ /*
+ SELECT title FROM film f
+ JOIN film_actor fa ON f.film_id=fa.film_id
+ JOIN actor a ON a.actor_id=fa.actor_id
+ WHERE a.first_name='Fred' AND a.last_name='Costner';*/ -- Question 21
+ 
+ -- SELECT DISTINCT language_id FROM film; Question 22
+ 
+-- SELECT * FROM language ORDER BY name DESC; Question 23
+ 
+-- SELECT * FROM actor WHERE last_name LIKE '%son'; Question 24
+ 
+ /* SELECT category_id, COUNT(category_id) AS value
+ FROM film_category GROUP BY category_id ORDER BY value DESC LIMIT 1;*/ Question 25
